956702988d05c6903515d39293d08701505383dc
hesoyam3178 <168158395+hesoyam3178@users.noreply.github.com>
2024-05-17 16:15:45 +0800

    Add files via upload

---
+++ New folder/myscripts.js
@@ -0,0 +1,65 @@
+var btnnvar=document.getElementById('btnh1');
+
+function Toggle1(){
+        if (btnnvar.style.color=="red"){
+            btnnvar.style.color="grey"
+        }
+        else{
+            btnnvar.style.color="red"
+        }
+}
+
+var btnnvar2=document.getElementById('btnh2');
+
+function Toggle2(){
+        if (btnnvar2.style.color=="red"){
+            btnnvar2.style.color="grey"
+        }
+        else{
+            btnnvar2.style.color="red"
+        }
+}
+
+var btnnvar3=document.getElementById('btnh3');
+
+function Toggle3(){
+        if (btnnvar3.style.color=="red"){
+            btnnvar3.style.color="grey"
+        }
+        else{
+            btnnvar3.style.color="red"
+        }
+}
+
+var btnnvar4=document.getElementById('btnh4');
+
+function Toggle4(){
+        if (btnnvar4.style.color=="red"){
+            btnnvar4.style.color="grey"
+        }
+        else{
+            btnnvar4.style.color="red"
+        }
+}
+
+var btnnvar5=document.getElementById('btnh5');
+
+function Toggle5(){
+        if (btnnvar5.style.color=="red"){
+            btnnvar5.style.color="grey"
+        }
+        else{
+            btnnvar5.style.color="red"
+        }
+}
+
+var btnnvar6=document.getElementById('btnh6');
+
+function Toggle6(){
+        if (btnnvar6.style.color=="red"){
+            btnnvar6.style.color="grey"
+        }
+        else{
+            btnnvar6.style.color="red"
+        }
+}
